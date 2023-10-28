9a98e7ae9d9e9cd43bc3e7634cf13c154bd8383c
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-10-28 10:18:03 -0300

    feat: implements events in socket for updateConnections in users

--- src/presentation/socket/index.ts
+++ src/presentation/socket/index.ts
@@ -4,6 +4,8 @@ import { Server } from 'socket.io';
 import type { MessageBody } from '../../shared/types/room';
 
 const createSocketServer = (server: ServerType) => {
+  let connectedUsers: string[] = [];
+
   const socketServer = new Server(server!, {
     cors: {
       origin: '*'
@@ -12,10 +14,24 @@ const createSocketServer = (server: ServerType) => {
 
   socketServer.on('connection', socket => {
     socket.on('sendMessage', (data: MessageBody) => {
-      socket.broadcast.emit('receiveMessages', data);
+      socket.broadcast.emit('updateMessages', data);
     });
     socket.on('createRoom', data => {
-      socket.broadcast.emit('resolveNewRooms', data);
+      socket.broadcast.emit('updateRooms', data);
+    });
+
+    socket.on('saveConnection', data => {
+      connectedUsers.push(data);
+      socketServer.emit('updateConnections', connectedUsers);
+    });
+
+    socket.on('removeConnection', data => {
+      connectedUsers = connectedUsers.filter(id => id !== data);
+      socket.broadcast.emit('updateConnections', connectedUsers);
+    });
+
+    socket.on('getConnections', callback => {
+      callback(connectedUsers);
     });
   });
 };
